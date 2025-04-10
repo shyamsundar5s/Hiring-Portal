90380cf99d48e23f7e0bee5d86c541b9cc7117a3
Shyamsundar Subramani <shyams0162@gmail.com>
2025-04-11 01:05:12 +0530

    Create ResumeTips.js

---
+++ ResumeTips.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ResumeTips = () => (
+  <div>
+    <h2>Resume Tips & Templates</h2>
+    <ul>
+      <li>Use a clean and professional format</li>
+      <li>Highlight relevant experiences</li>
+      <li>Tailor your resume to the job</li>
+    </ul>
+    <a href="/templates/basic-template.docx" download>Download Template</a>
+  </div>
+);
+
+export default ResumeTips;
